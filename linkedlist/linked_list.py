54ad03e28046fd66110cf9024881769b3bbdfa16
Dhiraj Singh <dhirajsingh_29@outlook.com>
2022-08-13 04:13:39 +0530

    merge two sorted list

---
+++ linkedlist/linked_list.py
@@ -0,0 +1,39 @@
+from typing import Optional
+
+class Node:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class SingleLinkedList:
+
+    def insert_node_at_tail(self, head: Optional[Node], x: int) -> Node:
+        temp = Node(x)
+
+        if head is None:
+            return temp
+
+        cur = head
+        while cur.next is not None:
+            cur = cur.next
+
+        cur.next = temp
+
+        return head
+
+    def insert_node_at_head(self, head: Optional[Node], x: int) -> Node:
+        temp = Node(x)
+
+        temp.next = head
+        head = temp
+
+        return head
+
+    def display(self, head: Optional[Node]) -> None:
+        if head is None:
+            print('List is empty')
+            return
+
+        while head is not None:
+            print(head.val, end=' ')
+            head = head.next
